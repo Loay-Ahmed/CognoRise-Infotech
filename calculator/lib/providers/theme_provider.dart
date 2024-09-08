c5078a2b46b0bae023fc07cc17e760c2e4eadb18
HP USER <loayabelgendy@gmail.com>
2024-09-08 21:51:15 +0300

    added launcher icon and finished core functionality

--- calculator/lib/providers/theme_provider.dart
+++ calculator/lib/providers/theme_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  bool isDark = true;
+  bool isDark = false;
 
   void toggleTheme() {
     isDark = !isDark;
